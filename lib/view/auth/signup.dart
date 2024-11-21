3026c421e2d47a943b2d7402c7d79e6198f08eb7
patipan-patisampita <patipan-patisampita@gmail.com>
2024-11-21 14:20:16 +0700

    update

---
+++ lib/view/auth/signup.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Signup extends StatefulWidget {
+  const Signup({super.key});
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                //==== Image register ====
+                Image.asset('assets/images/signup.png', width: size.width - 50)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
